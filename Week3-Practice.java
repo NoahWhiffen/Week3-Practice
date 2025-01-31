80f1a433605cc658b6446e24d645441e46129aaa
NoahWhiffen <Noah.Whiffen@keyin.com>
2025-01-31 11:36:50 -0330

    MyCircle class and methods completed

--- Week3-Practice.java
+++ Week3-Practice.java
@@ -1,6 +1,6 @@
 // Practice - Week 3
 // Author: Noah Whiffen - SD 12
-//Date: January 30th, 2025
+//Dates: January 30th, 2025 - 
 
 // Circle class
 class Circle {
@@ -108,8 +108,6 @@ class MyPoint {
 
 // MyCircle class (Different class than circle above)
 
-
-
 class MyCircle {
     private MyPoint center;
     private int radius;
@@ -117,5 +115,76 @@ class MyCircle {
     public MyCircle() {
         this.center = new MyPoint(0, 0);
         this.radius = 1;
+    }
+
+    // Creates a circle from x, y coordinates and radius
+    public MyCircle(int x, int y, int radius) {
+        this.center = new MyPoint(x, y);
+        this.radius = radius;
+    }
+
+    // Creates a circle usingan existing MyPoint object for the center, and a radius
+    public MyCircle(MyPoint center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public MyPoint getCenter() {
+        return center;
+    }
+
+    public void setCenter(MyPoint center) {
+        this.center = new MyPoint();
+    }
+
+    public int getCenterX(int x) {
+        return x;
+    }
+
+    public void setCenterX(int x) {
+        center.setX(x);
+    }
+
+    public int getCenterY(int y) {
+        return y;
+    }
+
+    public void setCenterY(int y) {
+        center.setY(y);
+    }
+
+    public int getCenterXY(MyPoint center) {
+        return center.getX(x) + center.getY(y);
+    }
+
+    public void setCenterXY(int x, int y) {
+        center.setX(x);
+        center.setY(y);
+    }
+
+    public String toString() {
+        return "My Circle [radius = " + radius + ", " + center + "]"; 
+    }
+
+    public double getArea() {
+        double area = Math.PI * Math.pow(radius, 2);
+        return area;
+    }
+
+    public double getCircumference() {
+        double circumference = 2 * Math.PI * radius;
+        return circumference;
+    }
+
+    public double distanceToCenter(MyCircle anotherCircle) {
+        return this.center.distance(anotherCircle.center);
     }
 }
---
+++ testMyCircle.java
@@ -0,0 +1,54 @@
+// Practice - Week 3
+// Author: Noah Whiffen - SD 12
+//Date: January 31th, 2025
+
+public class testMyCircle {
+
+    public static void main(String[] args) {
+
+        MyPoint center1 = new MyPoint(2, 3);
+        MyPoint center2 = new MyPoint(5, 7);
+
+        // Create MyCircle objects
+        MyCircle circle1 = new MyCircle(); // Default circle
+        MyCircle circle2 = new MyCircle(3, 4, 5);
+        MyCircle circle3 = new MyCircle(center1, 10);
+        MyCircle circle4 = new MyCircle(center2, 8); 
+
+        // Test setters
+        circle1.setRadius(7); 
+        circle1.setCenterXY(2, 3);
+        circle1.setCenterX(4); 
+        circle1.setCenterY(5);
+
+        // Test toString()
+        System.out.println("---------------------------------");
+        System.out.println("To String");
+        System.out.println("---------------------------------");
+        System.out.println(circle1);
+        System.out.println(circle2); 
+        System.out.println(circle3);
+        System.out.println(circle4);
+        System.out.println("---------------------------------");
+
+        // Testing getters
+        System.out.println("Getters");
+        System.out.println("---------------------------------");
+        System.out.println("Area of Circle 2: " + circle2.getArea());
+        System.out.println("Circumference of Circle 2: " + circle2.getCircumference());
+        System.out.println("Center of Circle 2: " + circle2.getCenter());
+        System.out.println("Center X of Circle 2: " + circle2.getCenterX(3)); 
+        System.out.println("Center Y of Circle 2: " + circle2.getCenterY(4));
+        System.out.println("---------------------------------");
+
+        // Testing the distanceToCenter method
+        System.out.println("Distance method");
+        System.out.println("---------------------------------");
+        System.out.println("Distance between Circle 1 and Circle 3 centers: " + circle1.distanceToCenter(circle3));
+
+        // Checking distance between two circles using their centers
+        double distance = circle2.distanceToCenter(circle4);
+        System.out.println("Distance between Circle 2 and Circle 4: " + distance);
+        System.out.println("---------------------------------");
+    }
+}
